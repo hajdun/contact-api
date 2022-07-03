4aa28a28d8b2b08b986c3bc8106ed95c5f694d81
DESKTOP-VI6LSH9\NoraHajdu <hajdu.nora115@gmail.com>
2022-07-03 12:56:59 +0200

    Sort, filter contacts

--- index.js
+++ index.js
@@ -1,80 +1,121 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const cors = require('cors');
+const cors = require("cors");
 
-app.use(cors())
+app.use(cors());
 
 // parse application/json
 app.use(bodyParser.json());
 
 const port = 3001;
 const frontendPort = 4200;
-const origin= `http://localhost:${frontendPort}`
+const origin = `http://localhost:${frontendPort}`;
 
 // the "database" (id "generated", name set somewhere on the UI)
 let contacts = [
   {
     id: 1,
-    name: "John Customer",
+    name: "John contact",
     phone: "+26 1 334 34 123",
   },
   {
     id: 2,
-    name: "Lia Customer",
+    name: "Lia contact",
     phone: "+26 1 334 34 124",
   },
 ];
 
-
-
-
-// retrieve all contacts on a GET http://localhost:3001/contacts request
-app.get("/contacts", (req, res) => {
-  res.set("Access-Control-Allow-Origin", origin);
-  res.json(contacts);
-});
-
-// save a new customer to the customer table on a POST http://localhost:3001/contacts request
-app.post("/contacts", (req, res, next) => {
-  const lastCustomer =
-    0 < contacts.length ? contacts[contacts.length - 1] : null;
-  const newCustomerId = lastCustomer ? lastCustomer.id + 1 : 1;
-
-//validate if phone number is a duplicate
-const foundSavedPhoneNumber=contacts.find(item=>item.phone===req.body.phone)
-res.set("Access-Control-Allow-Origin", origin);
-
-if(foundSavedPhoneNumber){
-  next("Error adding contact, phone number already exists for user named " + foundSavedPhoneNumber.name)
-} else {
-  contacts.push({
-    id: newCustomerId,
-    name: req.body.name,
-    phone: req.body.phone,
+const sortContacts=(array)=>{
+ return array.sort((a, b) => {
+    return a.name - b.name;
   });
-
-  res.json(contacts);
 }
 
+
+//validate if phone number is a duplicate
+const isDuplicatePhoneNumber = (phoneNumber) => {
+  const foundSavedPhoneNumber = contacts.find(
+    (item) => item.phone === phoneNumber
+  );
+  if (foundSavedPhoneNumber) {
+    return true;
+  }
+  return false;
+};
+
+// retrieve all contacts on a GET http://localhost:3001/contacts request
+// filter contacts by name GET http://localhost:3001/contacts request
+app.get("/contacts", (req, res) => {
+  const filterName = req.query.filter;
+  let filteredContacts = contacts;
+
+  if (filterName)
+    filteredContacts = contacts.filter((contact) => {
+      const lowerCaseContactArrayName = contact.name.toLowerCase().trim();
+      const lowerCaseFiltername = filterName.toLowerCase().trim();
+      return lowerCaseContactArrayName.indexOf(lowerCaseFiltername) > -1;
+    });
+
+  const sortedFilteredContacts = sortContacts(filteredContacts)
+
+  res.set("Access-Control-Allow-Origin", origin);
+  res.json(sortedFilteredContacts);
 });
 
-// updates an existing customer in the customer table on a PUT http://localhost:3001/contacts/[customerId] request
+// save a new contact to the contact table on a POST http://localhost:3001/contacts request
+app.post("/contacts", (req, res, next) => {
+  const lastcontact =
+    0 < contacts.length ? contacts[contacts.length - 1] : null;
+  const newcontactId = lastcontact ? lastcontact.id + 1 : 1;
+
+  //validate if phone number is a duplicate
+  const foundSavedPhoneNumber = isDuplicatePhoneNumber(req.body.phone);
+  res.set("Access-Control-Allow-Origin", origin);
+
+  if (foundSavedPhoneNumber) {
+    next(
+      "Error adding contact, phone number already exists for user named " +
+        foundSavedPhoneNumber.name
+    );
+  } else {
+    contacts.push({
+      id: newcontactId,
+      name: req.body.name,
+      phone: req.body.phone,
+    });
+    const sortedContacts = sortContacts(contacts)
+    res.json(sortedContacts);
+  }
+});
+
+// updates an existing contact in the contact table on a PUT http://localhost:3001/contacts/[contactId] request
 app.put("/contacts/:id", (req, res) => {
   const idFromParameter = req.params.id.toString();
-  const modifiedCustomerIndex = contacts.findIndex((element) => {
+  const modifiedcontactIndex = contacts.findIndex((element) => {
     return element.id.toString() === idFromParameter;
   });
 
-  contacts[modifiedCustomerIndex] = {
-    ...contacts[modifiedCustomerIndex],
-    ...req.body,
-  };
+  //validate if phone number is a duplicate
+  const foundSavedPhoneNumber = isDuplicatePhoneNumber(req.body.phone);
   res.set("Access-Control-Allow-Origin", origin);
-  res.json(contacts);
+
+  if (foundSavedPhoneNumber) {
+    next(
+      "Error updating contact, phone number already exists for user named " +
+        foundSavedPhoneNumber.name
+    );
+  } else {
+    contacts[modifiedcontactIndex] = {
+      ...contacts[modifiedcontactIndex],
+      ...req.body,
+    };
+    const sortedContacts = sortContacts(contacts)
+    res.json(sortedContacts);
+  }
 });
 
-// deletes an existing customer in the customer table on a DELETE http://localhost:3001/contacts/[customerId] request
+// deletes an existing contact in the contact table on a DELETE http://localhost:3001/contacts/[contactId] request
 app.delete("/contacts/:id", (req, res) => {
   const idFromParameter = req.params.id.toString();
 
@@ -82,7 +123,8 @@ app.delete("/contacts/:id", (req, res) => {
     return element.id.toString() != idFromParameter;
   });
   res.set("Access-Control-Allow-Origin", origin);
-  res.json(contacts);
+  const sortedContacts = sortContacts(contacts)
+  res.json(sortedContacts);
 });
 
 app.listen(port, () => {
